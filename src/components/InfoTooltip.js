6a766f5f2e5a4d175c26cfcab43b091979935640
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-04-19 02:15:09 +0700

    Sprint 12. Task2 in progress.

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,11 +1,13 @@
-import okImage from "../images/ok.svg";
-export default function InfoTooltip() {
+
+export default function InfoTooltip(props) {
   return (
     <div className={"popup"}>
       <div className="popup__container">
         <button className="popup__close-button link" aria-label="Close" type="button"></button>
-        <img src={okImage} alt="Вы успешно зарегистрировались"></img>
-        <h2 className="popup__title">Вы успешно зарегистрировались!</h2>
+        <div className="popup__info-wrapper">
+          <img className="popup__info-image" src={props.imgPath} alt={props.titleText} ></img>
+          <h2 className="popup__info-title">{props.titleText}</h2>
+        </div>
       </div>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import Form from './Form';
 import InfoTooltip from "./InfoTooltip";
+import okImage from "../images/ok.svg";
+import notOkImage from "../images/not-ok.svg";
 
 export default function Login() {
 
   const [loginSettings] = useState({
     title: "Вход",
-    buttonText: "Войти"
+    buttonText: "Войти",
+    titleTextOk: "Вы успешно зарегистрировались!",
+    titleTextNotOk: "Что-то пошло не так! Попробуйте еще раз."
   });
   return (
     <div className="login">
       <Form title={loginSettings.title} buttonText={loginSettings.buttonText} />
-      <InfoTooltip />
+      <InfoTooltip imgPath={okImage} titleText={loginSettings.titleTextOk} />
+      <InfoTooltip imgPath={notOkImage} titleText={loginSettings.titleTextNotOk} />
     </div>
   )
 }
